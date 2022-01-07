3ff6b0cf139c0bb04df7ab49fc66f3b48bb2b987
Dmitry Zavyalov <zavyalovdmitry@mail.ru>
2022-01-07 10:27:34 -0500

    fix: env var

--- src/mockAPI.js
+++ src/mockAPI.js
@@ -1,28 +1,29 @@
-const API = 'https://60bad97042e1d00017620127.mockapi.io';
+const API = process.env.REACT_APP_API;
+
 const HEADERS = {
-   'Content-Type': 'application/json;charset=utf-8'
+  'Content-Type': 'application/json;charset=utf-8',
 };
 
 export const subjectsAndTests = async (endPoint) => {
   try {
-    const response = await fetch(`${API}/${endPoint}`)
+    const response = await fetch(`${API}/${endPoint}`);
     if (response.ok) {
-        const data = await response.json();
-        return data;
+      const data = await response.json();
+      return data;
     }
   } catch (error) {
     throw error;
   }
-}
+};
 
 export const levelsData = async () => {
   try {
-    const response = await fetch(`${API}/Levels`)
+    const response = await fetch(`${API}/Levels`);
     if (response.ok) {
-        const data = await response.json();
-        return data;
+      const data = await response.json();
+      return data;
     }
   } catch (error) {
     throw error;
   }
-}
+};
